224c8ce5ac915203697542e2a4fbcf295ab89459
Suraj Meghwar <isurajparmar85@gmail.com>
2024-06-03 23:04:36 -0700

    Initial Commit

---
+++ Challenge4-6Tests/Challenge4_6Tests.swift
@@ -0,0 +1,33 @@
+//
+//  Challenge4_6Tests.swift
+//  Challenge4-6Tests
+//
+//  Created by isurajparmar85 on 03/06/2024.
+//
+
+import XCTest
+@testable import Challenge4_6
+
+class Challenge4_6Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
